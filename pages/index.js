1d6c1729293d3ebbca00a5d19660889b41152a31
Inessa Kim <kim.inessa20@yandex.ru>
2023-05-26 20:05:16 +0300

    fix: исправлены замечания

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,38 @@
-import { form } from "../utils/constants.js";
-import { handleFormSubmit } from "../utils/utils.js";
+const block = document.querySelector(".block");
+const form = block.querySelector(".block__form");
+const btnSubmit = block.querySelector(".block__btn");
+const screen = document.querySelector(".screen");
+const screenText = screen.querySelector(".screen__text");
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  const collection = form.elements;
+  let formData = {};
+
+  for (let i = 0; i < collection.length - 1; i++) {
+    formData[collection[i].name] = collection[i].value;
+  }
+
+  const jsonStructure = JSON.stringify(formData);
+  screenText.textContent = jsonStructure;
+
+  fetch("https://someUrl.com", {
+    method: "GET",
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        return Promise.reject(`Что-то пошло не так: ${res.status}`);
+      }
+    })
+    .then((data) => {
+      alert(data);
+    })
+    .catch((err) => {
+      alert(err);
+    });
+}
 
 form.addEventListener("submit", (e) => {
   handleFormSubmit(e);
